646f4f7dc4ecdc7dbdbda1ea5e7852a853bdff26
Sutrisno Efendi <kangfend@gmail.com>
2017-02-08 14:37:36 +0700

    Create setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='bahasa',
+    packages=['bahasa'],
+    version='0.0.1',
+    description='Bahasa is natural language toolkit for bahasa indonesia',
+    license='MIT',
+    author='Sutrisno Efendi',
+    author_email='kangfend@gmail.com',
+    url='https://github.com/kangfend/bahasa',
+    keywords=['NLP', 'Bahasa', 'Indonesia', 'Stemmer'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alph'a
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Natural Language :: Indonesian',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
